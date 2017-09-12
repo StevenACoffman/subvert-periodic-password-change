8d405d68214913ab77cdda322cc53a6baa146bf0
Steve Coffman <steve.coffman@ithaka.org>
2017-09-12 16:10:22 -0400

    Docker hub required repo name less than 50 characters

--- make_docker.sh
+++ make_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # USAGE:  $1 = docker registry repository prefix - e.g. prod, qa, etc.
 #
-NAME_OF_THING=active-directory-subvert-periodic-password-change
+NAME_OF_THING=subvert-periodic-password-change
 DR_RP=${1:-stevenacoffman}
 img="${DR_RP}/${NAME_OF_THING}:latest"
 docker build -t ${img} .
